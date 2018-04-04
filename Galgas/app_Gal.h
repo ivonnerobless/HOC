1438e86716508c9c01f8ba35abd11fb6cd5ed56d
luisi_000 <luisi_000@TOSHIBA>
2018-04-04 12:32:27 -0500

    App Galgas Added to preoject

---
+++ Galgas/app_Gal.h
@@ -0,0 +1,20 @@
+/*
+ * app_Gal.h
+ *
+ *  Created on: 02/04/2018
+ *      Author: luisi_000
+ */
+
+#ifndef APP_GAL_H_
+#define APP_GAL_H_
+
+
+/******************************************
+ * Public Prototypes
+ ******************************************/
+
+extern void app_ADC_Init (void);
+extern void app_ADC_Task(void);
+
+
+#endif /* APP_GAL_H_ */
