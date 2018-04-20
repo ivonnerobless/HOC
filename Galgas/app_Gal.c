2c88b5d6916ed7942ff8a08573ade323ed765cc9
luisi_000 <luisi_000@TOSHIBA>
2018-04-19 22:45:27 -0500

    SW_Galgas

--- Galgas/app_Gal.c
+++ Galgas/app_Gal.c
@@ -12,7 +12,6 @@
 #include "stdtypedef.h"
 #include "fsl_adc16.h"
 #include "app_Gal.h"
-#include "app_LED.h"
 
 
  /******************************************
@@ -20,27 +19,29 @@
  ******************************************/
 
 #define APP_ADC_CHANNEL_GROUP 	0u
-#define APP_ADC_Gal1_CHANNEL 	0u
-//#define APP_ADC_Gal2_CHANNEL    4u
-//#define APP_ADC_Gal3_CHANNEL    3u
-//#define APP_ADC_Gal4_CHANNEL    7u
+
+#define APP_Gal1_CHANNEL 	0u
+#define APP_Gal2_CHANNEL    4u
+#define APP_Gal3_CHANNEL    3u
+#define APP_Gal4_CHANNEL    7u
 
  /******************************************
  * Private Variables
  ******************************************/
-static T_UWORD ruw_ADCValue = 0u;
+static T_UWORD ruw_ADCValue[4];
 static T_UBYTE rub_ConversionInProgressFlag = FALSE;
+
+
+
 
 
  /******************************************
  * Private Prototypes
  ******************************************/
-static void app_ADC_Gal1_Trigger(void);
-/*static void app_ADC_Gal2_Trigger(void);
-static void app_ADC_Gal3_Trigger(void);
-static void app_ADC_Gal4_Trigger(void);
+static void app_Gal_Trigger(unsigned int Channel);
+
 static T_UBYTE app_ADC_IsConversionCompleted(void);
-static T_UWORD app_ADC_GetValue(void);*/
+static T_UWORD app_ADC_GetValue(void);
 
  /******************************************
  * Code
@@ -48,7 +49,7 @@ static T_UWORD app_ADC_GetValue(void);*/
 
 /***********************************************
  * Initialization of the ADC module
- * Function Name: app_ADC_Gal1_Init
+ * Function Name: app_Gal1_Init
  ***********************************************/
 
 void app_ADC_Init(void)
@@ -70,40 +71,14 @@ void app_ADC_Init(void)
 
 /***********************************************
  * Generate trigger to start conversion
- * Function Name: app_ADC_Gal2_ Trigger
+ * Function Name: app_Gal2_ Trigger
  ***********************************************/
-
-/*static void app_ADC_Gal2_Trigger(void)
+static void app_Gal_Trigger(unsigned int Channel )
 {
 	adc16_channel_config_t ls_ChannelConfig;
 
 	//Channel Selection
-	ls_ChannelConfig.channelNumber = APP_ADC_Gal2_CHANNEL;
-
-	//Disable Interrupt when Conversion is completed
-	ls_ChannelConfig.enableInterruptOnConversionCompleted = FALSE;
-
-	//Disable Differential Conversion
-	ls_ChannelConfig.enableDifferentialConversion = FALSE;
-
-	ADC16_SetChannelConfig(ADC0, APP_ADC_CHANNEL_GROUP, &ls_ChannelConfig);
-}
-
-*/
-
-
-
-/***********************************************
- * Generate trigger to start conversion
- * Function Name: app_ADC_Gal1_ Trigger
- ***********************************************/
-
-static void app_ADC_Gal1_Trigger(void)
-{
-	adc16_channel_config_t ls_ChannelConfig;
-
-	//Channel Selection
-	ls_ChannelConfig.channelNumber = APP_ADC_Gal1_CHANNEL;
+	ls_ChannelConfig.channelNumber = Channel;
 
 	//Disable Interrupt when Conversion is completed
 	ls_ChannelConfig.enableInterruptOnConversionCompleted = FALSE;
@@ -115,54 +90,9 @@ static void app_ADC_Gal1_Trigger(void)
 }
 
 
-
-/***********************************************
- * Generate trigger to start conversion
- * Function Name: app_ADC_Gal3_ Trigger
- ***********************************************/
-
-/*static void app_ADC_Gal3_Trigger(void)
-{
-	adc16_channel_config_t ls_ChannelConfig;
-
-	//Channel Selection
-	ls_ChannelConfig.channelNumber = APP_ADC_Gal3_CHANNEL;
-
-	//Disable Interrupt when Conversion is completed
-	ls_ChannelConfig.enableInterruptOnConversionCompleted = FALSE;
-
-	//Disable Differential Conversion
-	ls_ChannelConfig.enableDifferentialConversion = FALSE;
-
-	ADC16_SetChannelConfig(ADC0, APP_ADC_CHANNEL_GROUP, &ls_ChannelConfig);
-}
-*/
-
-/***********************************************
- * Generate trigger to start conversion
- * Function Name: app_ADC_Gal4_ Trigger
- ***********************************************/
-
-/*static void app_ADC_Gal4_Trigger(void)
-{
-	adc16_channel_config_t ls_ChannelConfig;
-
-	//Channel Selection
-	ls_ChannelConfig.channelNumber = APP_ADC_Gal4_CHANNEL;
-
-	//Disable Interrupt when Conversion is completed
-	ls_ChannelConfig.enableInterruptOnConversionCompleted = FALSE;
-
-	//Disable Differential Conversion
-	ls_ChannelConfig.enableDifferentialConversion = FALSE;
-
-	ADC16_SetChannelConfig(ADC0, APP_ADC_CHANNEL_GROUP, &ls_ChannelConfig);
-}
-
-*/
 /***********************************************
  * Waiting flag or conversion interruption
- * Function Name: app_ADC_Gal1_IsConversionCompleted
+ * Function Name: app_Gal1_IsConversionCompleted
  ***********************************************/
 
 static T_UBYTE app_ADC_IsConversionCompleted(void)
@@ -189,7 +119,7 @@ static T_UBYTE app_ADC_IsConversionCompleted(void)
 
 /***********************************************
  * Read conversion value
- * Function Name: app_ADC_Gal1_GetValue
+ * Function Name: app_Gal1_GetValue
  ***********************************************/
 
 static T_UWORD app_ADC_GetValue(void)
@@ -203,51 +133,58 @@ static T_UWORD app_ADC_GetValue(void)
  ***********************************************/
 void app_ADC_Task(void)
 {
-	//Check if a conversion is in progress
-	if(TRUE == rub_ConversionInProgressFlag)
+	int i=0;
+	while(1)
 	{
-		//Check if Conversion was completed
-		if(TRUE == app_ADC_IsConversionCompleted())
+		//Check if a conversion is in progress
+		if(TRUE == rub_ConversionInProgressFlag)
 		{
-			//Store the ADC Value
-			ruw_ADCValue = app_ADC_GetValue();
+			//Check if Conversion was completed
+			if(TRUE == app_ADC_IsConversionCompleted())
+			{
+				//Store the ADC Value
+				ruw_ADCValue[i] = app_ADC_GetValue();
 
-			if(ruw_ADCValue <= 1365)
-			{
-			LED_RED_OFF();
-			LED_BLUE_OFF();
-			LED_GREEN_ON();
-			}
-			else if(ruw_ADCValue > 1365 && ruw_ADCValue <= 2730)
-			{
-			LED_RED_OFF();
-		    LED_BLUE_ON();
-			LED_GREEN_OFF();
-			}
-			else if(ruw_ADCValue > 2730)
-			{
-			LED_RED_ON();
-		    LED_BLUE_OFF();
-			LED_GREEN_OFF();
-			}
 
-			//Clear conversion in progress flag
-			rub_ConversionInProgressFlag = FALSE;
+
+
+				//Clear conversion in progress flag
+				rub_ConversionInProgressFlag = FALSE;
+				i++;
+				if(i>4)
+					i=0;
+			}
+			else
+			{
+				/* Do nothing */
+			}
 		}
 		else
 		{
-			/* Do nothing */
-		}
-	}
-	else
-	{
-		//Trigger the ADC Conversion
-		app_ADC_Gal1_Trigger();
-		//app_ADC_Gal2_Trigger();
-		//app_ADC_Gal3_Trigger();
-		//app_ADC_Gal4_Trigger();
+			//Trigger the ADC Conversion
+			if(i==0)
+				app_Gal_Trigger(APP_Gal1_CHANNEL);
 
-		//Set Conversion in progress flag
-		rub_ConversionInProgressFlag = TRUE;
+			else if (i==1)
+				app_Gal_Trigger(APP_Gal2_CHANNEL);
+
+			else if (i==2)
+				app_Gal_Trigger(APP_Gal3_CHANNEL);
+
+			else if (i==3)
+				app_Gal_Trigger(APP_Gal4_CHANNEL);
+
+			ruw_ADCValue[i] = app_ADC_GetValue();
+
+
+
+
+			//Set Conversion in progress flag
+			rub_ConversionInProgressFlag = TRUE;
+		}
+
+
 	}
+
+
 }
