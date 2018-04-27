4b0337f44f78b307852466ae8c167af0b299bd7d
luisi_000 <luisi_000@TOSHIBA>
2018-04-26 21:28:50 -0500

    Speed ​​calculation software with tests.

--- SW_Calculos_Speed/SW_Calculos_Speed.c
+++ SW_Calculos_Speed/SW_Calculos_Speed.c
@@ -5,31 +5,62 @@
  *      Author: Ivonne Robles
  */
 
+ /******************************************
+ * Interfaces
+ ******************************************/
 #include "stdtypedef.h"
+#include "SW_Calculos_Speed.h"
+
+
+/******************************************
+ * Private Macros
+ ******************************************/
+#define rub_Force 0u
+
+/******************************************
+ * Private Prototypes
+ ******************************************/
+static T_UBYTE app_Speed_Force2Speed(T_UBYTE lub_Force);
+
+
 
 T_UBYTE rub_Speed;
 
+/***********************************************
+ * Initialization of the Speed module
+ * Function Name: app_Speed_Init
+ ***********************************************/
+
+
 void app_Speed_Init(void)
 {
-	/* Module Init */
 
 	//Variables
 	rub_Speed = MACRO_SPEED_STOP_VALUE;
-	//States
-	//Flags
-	//HW
+
 }
+
+/***********************************************
+ * Read conversion value
+ * Function Name:app_Speed_Force2Speed
+ ***********************************************/
+
 
 T_UBYTE app_Speed_Force2Speed(T_UBYTE lub_Force)
 {
 	return (T_UBYTE)(lub_Force * MACRO_SPEED_MULTIPLIER);
 }
 
+
+
+/***********************************************
+ * Function Name: app_Speed_MngTask
+ ***********************************************/
 void app_Speed_MngTask(void)
 {
 	if(FALSE == MACRO_SPEED_ISANOBSTACLE)
 	{
-		rub_Speed = app_Speed_Force2Speed(rub_Force);
+		rub_Speed = app_Speed_Force2Speed(rub_Force);																																																																																																																							rub_Speed = app_Speed_Force2Speed(rub_Force);
 	}
 	else
 	{
