fa20750eb41e7ea9da721a08b2b7083c91c821ce
Ivonne Robles <Ivonne Robles@ivonne>
2018-04-30 17:00:10 -0500

    new

---
+++ SW_Calculos_Direction/SW_Calculos_Direction.c
@@ -0,0 +1,48 @@
+/*
+ * SW_Calculos_Direction.c
+ *
+ *  Created on: 30/04/2018
+ *      Author: Ivonne Robles
+ */
+
+#include "SW_Calculos_Direction.h"
+#include "MKL25Z4.h"
+#include "fsl_common.h"
+#include "fsl_gpio.h"
+#include "fsl_port.h"
+#include "fsl_clock.h"
+#include "fsl_pit.h"
+#include "SW_Calculos_Direction.h"
+#include "stdtypedef.h"
+
+T_UBYTE rub_front_left;
+T_UBYTE rub_front_right;
+T_UBYTE rub_rear_left;
+T_UBYTE rub_rear_right;
+T_UBYTE rub_center;
+T_UBYTE rub_counter_weight;
+T_UBYTE rub_sensor_direction;
+
+
+
+
+void app_direction(void){
+
+
+
+
+	rub_front_left;
+	rub_front_right;
+	rub_rear_left;
+	rub_rear_right;
+    rub_center;
+
+
+
+
+
+
+
+
+}
+
---
+++ SW_Calculos_Direction/SW_Calculos_Direction.h
@@ -0,0 +1,13 @@
+/*
+ * SW_Calculos_Direction.h
+ *
+ *  Created on: 30/04/2018
+ *      Author: Ivonne Robles
+ */
+
+#ifndef SW_CALCULOS_DIRECTION_H_
+#define SW_CALCULOS_DIRECTION_H_
+
+
+
+#endif /* SW_CALCULOS_DIRECTION_H_ */
