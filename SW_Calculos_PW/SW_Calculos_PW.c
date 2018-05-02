1bc7e5a3bf0301dd9072bae2f6b3bbe4ad4dff7c
Ivonne Robles <Ivonne Robles@192.168.1.74>
2018-05-01 19:50:31 -0500

    Calculos PW

---
+++ SW_Calculos_PW/SW_Calculos_PW.c
@@ -0,0 +1,8 @@
+/*
+ * SW_Calculos_PW.c
+ *
+ *  Created on: 01/05/2018
+ *      Author: Ivonne Robles
+ */
+
+
---
+++ SW_Calculos_PW/SW_Calculos_PW.h
@@ -0,0 +1,8 @@
+/*
+ * SW_Calculos_PW.h
+ *
+ *  Created on: 01/05/2018
+ *      Author: Ivonne Robles
+ */
+
+
